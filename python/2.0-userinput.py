a98831c4bfa6ddebdeae622c6e5fbfa74decf811
Lucifer <louie.francis@hotmail.com>
2018-08-22 15:26:30 +0100

    Edited Python script with user input

--- python/2.0-userinput.py
+++ python/2.0-userinput.py
@@ -1,10 +1,9 @@
 ### References - 'https://stackoverflow.com/questions/9257094/how-to-change-a-string-into-uppercase'
 ###              'https://www.pythonforbeginners.com/basics/getting-user-input-from-the-keyboard'
 
-### User input string
+### User input string (text)
+name = raw_input("Write your text to convert:\n")
 
-name = raw_input("Write your text to convert: ")
-
-### Print input string 'alphabet' as uppercase
-
+### Print input string as uppercase
+print "\nConverted output:"
 print (name.upper())
