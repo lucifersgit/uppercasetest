f48bcbfca386d1bb407c064f8d0dcab6d1f169c3
Lucifer <louie.francis@hotmail.com>
2018-08-22 12:14:20 +0100

    Updated file naming convention

---
+++ python/1.0-lowertoupperpython.py
@@ -0,0 +1,9 @@
+### This is sourced from 'https://stackoverflow.com/questions/9257094/how-to-change-a-string-into-uppercase'
+
+### Manual alphabet string
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+### Print lowercase string 'alphabet' as uppercase
+
+print (alphabet.upper())
