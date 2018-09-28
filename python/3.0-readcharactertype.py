2afdd0307b9cfce8afdf10d5031eb815ae70921c
Lucifer <louie.francis@hotmail.com>
2018-09-28 15:06:12 +0100

    changing the content to be suitable for Python3

--- python/3.0-readcharactertype.py
+++ python/3.0-readcharactertype.py
@@ -2,17 +2,17 @@
 ###              'https://www.pythonforbeginners.com/basics/getting-user-input-from-the-keyboard'
 
 ### User input string (text)
-name = raw_input("Write your text to confirm if it is uppercase or lowercase:\n")
+name = input("Write your text to confirm if it is uppercase or lowercase:\n")
 
 ### Determine if string is uppercase or lowercase, and print corresponding output:
 
 if name.isupper():
-    print "This is an uppercase word"
+    print("This is an uppercase word")
 
 elif name.islower():
-    print "This is a lowercase word"
+    print("This is a lowercase word")
 
 ### If characters are mixed, print error:
 
 else:
-    print "Invalid type - mixed character cases"
+    print ("Invalid type - mixed character cases")
